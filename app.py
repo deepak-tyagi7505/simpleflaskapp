247fecdadc41e0af284b96d6ae0c533ee99f9937
deepak-tyagi7505 <140254902+deepak-tyagi7505@users.noreply.github.com>
2023-12-04 23:37:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-import gunicorn
-import wfastcgi
-
+    
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __main__ == '__main__':
+    app.run()
